f046b403295bafa5e6e172e42dd20e557b77363e
Itay01 <itaymarom07@gmail.com>
2022-11-25 00:38:28 +0200

    Update Main.py

--- main.py
+++ main.py
@@ -1,97 +1,69 @@
 import requests
 from twilio.rest import Client
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
 
+VIRTUAL_TWILIO_NUMBER = "+14155238886"
+VERIFIED_NUMBER = "+972585120807"
 
-app = Flask(__name__)
+STOCK_NAME = "TSLA"
+COMPANY_NAME = "Tesla Inc"
 
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+STOCK_API_KEY = "5CG6W0ZGOUHWPAG0"
+NEWS_API_KEY = "e1dfb3794fac4d5f918e1aaacbd7ab5e"
+TWILIO_SID = "AC0d072a022d6c1935415814ce5f224292"
+TWILIO_AUTH_TOKEN = "dc2d77adb8ce91bb43695673891731df"
 
+stock_params = {
+    "function": "TIME_SERIES_DAILY_ADJUSTED",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY,
+}
 
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
 
-    # Create reply
-    resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
 
-    return str(resp)
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+print(day_before_yesterday_closing_price)
 
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
+print(diff_percent)
 
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
 
-# VIRTUAL_TWILIO_NUMBER = "+14155238886"
-# VERIFIED_NUMBER = "+972585120807"
-#
-# STOCK_NAME = "TSLA"
-# COMPANY_NAME = "Tesla Inc"
-#
-# STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-# NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
-#
-# STOCK_API_KEY = "5CG6W0ZGOUHWPAG0"
-# NEWS_API_KEY = "e1dfb3794fac4d5f918e1aaacbd7ab5e"
-# TWILIO_SID = "AC0d072a022d6c1935415814ce5f224292"
-# TWILIO_AUTH_TOKEN = "dc2d77adb8ce91bb43695673891731df"
-#
-# stock_params = {
-#     "function": "TIME_SERIES_DAILY_ADJUSTED",
-#     "symbol": STOCK_NAME,
-#     "apikey": STOCK_API_KEY,
-# }
-#
-# response = requests.get(STOCK_ENDPOINT, params=stock_params)
-#
-# data = response.json()["Time Series (Daily)"]
-# data_list = [value for (key, value) in data.items()]
-# yesterday_data = data_list[0]
-# yesterday_closing_price = yesterday_data["4. close"]
-# print(yesterday_closing_price)
-#
-# day_before_yesterday_data = data_list[1]
-# day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
-# print(day_before_yesterday_closing_price)
-#
-# difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
-# up_down = None
-# if difference > 0:
-#     up_down = "🔺"
-# else:
-#     up_down = "🔻"
-#
-# diff_percent = round((difference / float(yesterday_closing_price)) * 100)
-# print(diff_percent)
-#
-# if abs(diff_percent) > 1:
-#     news_params = {
-#         "apiKey": NEWS_API_KEY,
-#         "qInTitle": COMPANY_NAME,
-#     }
-#
-#     news_response = requests.get(NEWS_ENDPOINT, params=news_params)
-#     articles = news_response.json()["articles"]
-#
-#     three_articles = articles[:3]
-#     print(three_articles)
-#
-#     formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \n" \
-#                           f"Brief: {article['description']}" for article in three_articles]
-#     print(formatted_articles)
-#
-#     client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
-#
-#     for article in formatted_articles:
-#         message = client.messages.create(
-#             body=article,
-#             from_=f"whatsapp:{VIRTUAL_TWILIO_NUMBER}",
-#             to=f"whatsapp:{VERIFIED_NUMBER}"
-#         )
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+    print(three_articles)
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \n" \
+                          f"Brief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=f"whatsapp:{VIRTUAL_TWILIO_NUMBER}",
+            to=f"whatsapp:{VERIFIED_NUMBER}"
+        )
