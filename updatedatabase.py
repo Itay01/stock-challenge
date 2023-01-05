10266172dc6be26490a3ef452783f766be8c323d
Itay01 <itaymarom07@gmail.com>
2023-01-06 00:30:53 +0200

    Update database update_user_stocks function.

--- updatedatabase.py
+++ updatedatabase.py
@@ -7,8 +7,7 @@ from sqlalchemy.orm import relationship
 import pandas as pd
 import os
 from stockmessages import StockMessage
-import yfinance as yf
-import time
+# import time
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
@@ -97,40 +96,51 @@ def update_stocks_table():
 
 def update_user_stocks(user_id):
     """Update the user's stocks, and values. Send notification message if needed."""
-    # st = time.time()
     messages = StockMessage()
 
     user = User.query.get(user_id)
     user.stocks_value = 0
-    for stock in Stocks.query.filter_by(follower_id=user.id).all():
-        stock_follower = StockFollower(stock.stock_name)
-        try:
-            stock_follower.get_stock()
-            stock_follower.get_stock_diff(stock.stock_price)
-        except TypeError:
-            messages.send_message("itaymarom07@gmail.com", f"Error (update): {stock.stock_name}.")
-            user.stocks_value = user.stocks_value + stock.stock_units_value
-        except json.decoder.JSONDecodeError:
-            messages.send_message("itaymarom07@gmail.com", f"Error (update): {stock.stock_name}.")
-            user.stocks_value = user.stocks_value + stock.stock_units_value
-        else:
-            stock.stock_value = stock_follower.current_price
-            stock.stock_diff = stock_follower.diff_percent
-            stock.stock_units_value = stock.stock_value * stock.stock_units
-            user.stocks_value = user.stocks_value + stock.stock_units_value
+    updates_left = [stock for stock in Stocks.query.filter_by(follower_id=user.id).all()]
+    tries = 5
+    while updates_left and tries > 0:
+        for stock in updates_left:
+            stock_follower = StockFollower(stock.stock_name)
+            try:
+                stock_follower.get_stock()
+                stock_follower.get_stock_diff(stock.stock_price)
+            except (TypeError, json.decoder.JSONDecodeError):
+                user.stocks_value = user.stocks_value + stock.stock_units_value
+            else:
+                stock.stock_value = stock_follower.current_price
+                stock.stock_diff = stock_follower.diff_percent
+                stock.stock_units_value = stock.stock_value * stock.stock_units
+                user.stocks_value = user.stocks_value + stock.stock_units_value
 
-        # if user.email != os.environ.get("UNEMAIL"):
-        #     messages.send_message(user.email, f"{stock.stock_name}: {stock.stock_diff}", stock.stock_diff)
+                updates_left.remove(stock)
 
-    # db.session.commit()
-    # ed = time.time()
-    # print(ed - st)
+        tries -= 1
+
+    if updates_left:
+        messages.send_message("itaymarom07@gmail.com",
+                              f"Error (update): {', '.join([stock.stock_name for stock in updates_left])}.")
+
+    # if user.email != os.environ.get("UNEMAIL"):
+    #     messages.send_message(user.email, f"{stock.stock_name}: {stock.stock_diff}", stock.stock_diff)
 
 
 def check_diff():
     """Update all users stocks, and send notification messages if needed."""
     for user in User.query.all():
         update_user_stocks(user.id)
+    db.session.commit()
+
+
+def reset_user(user_id):
+    user = User.query.get(user_id)
+    for stock in Stocks.query.filter_by(follower_id=user.id).all():
+        db.session.delete(stock)
+    user.stock_points = 15000
+    user.stocks_value = 0
     db.session.commit()
 
 
